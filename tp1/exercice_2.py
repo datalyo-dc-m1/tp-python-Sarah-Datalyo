0585387148baa2ce354214577f032f78bb1ffee7
Sarah KEZIRE-LARCHER <skezire-larcher.externe@m6.fr>
2020-04-03 15:51:38 +0200

    tp1/exercice_2

---
+++ tp1/exercice_2.py
@@ -0,0 +1,14 @@
+# Affiche les nombres pairs entre 0 et 20
+
+print("En utilisant une boucle For: ")
+
+for i in range(0, 21, 2):
+    print(i)
+
+
+print("\nEn utilisant une boucle TantQue: ")
+
+i = 0
+while i <= 20:
+    print(i)
+    i = i + 2
